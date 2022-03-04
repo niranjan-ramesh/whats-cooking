729d7876cf1a208871c495fbae64bd5b9a223c53
niranjan-ramesh <nra38@sfu.ca>
2022-03-04 13:47:39 -0800

    Removed unnecessary code

--- food-scraper.py
+++ food-scraper.py
@@ -9,71 +9,22 @@ if not os.path.exists('source'):
     os.mkdir('source')
 
 import logging
-import random
-import threading
+from threading import Thread
 import time
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s (%(threadName)-2s) %(message)s',
                     )
 
-class ActivePool(object):
-    def __init__(self):
-        super(ActivePool, self).__init__()
-        self.active = []
-        self.lock = threading.Lock()
-    def makeActive(self, name):
-        with self.lock:
-            self.active.append(name)
-            logging.debug('Running: %s', self.active)
-    def makeInactive(self, name):
-        with self.lock:
-            self.active.remove(name)
-            logging.debug('Running: %s', self.active)
+def worker(name):
+    URL = 'https://www.food.com/recipe?pn={}'.format(name)
+    response = requests.get(URL)
+    source_file = open('source/{}.html'.format(name), 'w')
+    source_file.write(response.text)
+    source_file.close()
 
-def worker(s, pool):
-    logging.debug('Waiting to join the pool')
-    with s:
-        name = threading.currentThread().getName()
-        pool.makeActive(name)
-        URL = 'https://www.food.com/recipe?pn={}'.format(name)
-        response = requests.get(URL)
-        source_file = open('source/{}.html'.format(name), 'w')
-        source_file.write(response.text)
-        source_file.close()
-        pool.makeInactive(name)
-
-
-pool = ActivePool()
-s = threading.Semaphore(100)
 for i in range(52381):
     if((i%100) == 0):
-        time.sleep(20)
-    t = threading.Thread(target=worker, name=str(i), args=(s, pool))
+        time.sleep(5)
+    t = Thread(target=worker, args=(i, ))
     t.start()
-
-# op = webdriver.ChromeOptions()
-# op.add_argument('headless')
-
-# driver = webdriver.Chrome(executable_path=r"/Users/ninja/Downloads/chromedriver", options=op)
-# driver.get("https://www.food.com/recipe")
-# time.sleep(2)  
-# driver.find_element_by_css_selector('.gk-aa-load-more').click()
-# scroll_pause_time = 0.5
-# screen_height = driver.execute_script("return window.screen.height;")   
-# i = 1
-
-# while True:
-    
-#     driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
-#     i += 1
-#     time.sleep(scroll_pause_time)
-    
-#     scroll_height = driver.execute_script("return document.body.scrollHeight;")  
-    
-#     if (screen_height) * i > scroll_height:
-#         break
-
-# soup = BeautifulSoup(driver.page_source, "html.parser")
-# with open("output1.html", "w") as file:
-#     file.write(str(soup))
